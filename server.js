2a30a2a078b5d5ad9453929fa569debafaa2301c
zhangyuang <leonin0829@gmail.com>
2025-08-01 13:15:31 +0800

    feat: add ssrdevserver bug demo

--- server.js
+++ server.js
@@ -8,62 +8,50 @@ const isTest = process.env.VITEST
 
 export async function createServer(
   root = process.cwd(),
-  hmrPort,
-  customLogger,
+
 ) {
+  const clientConfig = {
+    root,
+    logLevel: 'error',
+    server: {
+      middlewareMode: true,
+    },
+    appType: 'custom',
+  }
+
+  const serverConfig = {
+    root,
+    logLevel: 'error',
+    server: {
+      middlewareMode: true,
+    },
+    appType: 'custom',
+  }
   const resolve = (p) => path.resolve(__dirname, p)
 
   const app = express()
-
-  /**
-   * @type {import('vite').ViteDevServer}
-   */
-  const vite = await (
-    await import('vite')
-  ).createServer({
-    root,
-    logLevel: isTest ? 'error' : 'info',
-    server: {
-      middlewareMode: true,
-      watch: {
-        // During tests we edit the files too fast and sometimes chokidar
-        // misses change events, so enforce polling for consistency
-        usePolling: true,
-        interval: 100,
-      },
-      hmr: {
-        port: hmrPort,
-      },
-    },
-    appType: 'custom',
-    customLogger,
-  })
+  const { createServer } = await import('vite')
+  const clientDevServer = await createServer(clientConfig)
+  const serverDevServer = await createServer(serverConfig)
   // use vite's connect instance as middleware
-  app.use(vite.middlewares)
+  app.use(clientDevServer.middlewares)
 
   app.use('*all', async (req, res, next) => {
-    try {
-      const url = req.originalUrl
+    const url = req.originalUrl
 
-      let template
-      template = fs.readFileSync(resolve('index.html'), 'utf-8')
-      template = await vite.transformIndexHtml(url, template)
-      const render = (await vite.ssrLoadModule('/src/app.js')).render
+    let template
+    template = fs.readFileSync(resolve('index.html'), 'utf-8')
+    template = await clientDevServer.transformIndexHtml(url, template)
+    const render = (await serverDevServer.ssrLoadModule('/src/app.js')).render
 
-      const appHtml = await render(url, __dirname)
+    const appHtml = await render(url, __dirname)
 
-      const html = template.replace(`<!--app-html-->`, appHtml)
+    const html = template.replace(`<!--app-html-->`, appHtml)
 
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
-    } catch (e) {
-      vite && vite.ssrFixStacktrace(e)
-      if (isTest) throw e
-      console.log(e.stack)
-      res.status(500).end(e.stack)
-    }
+    res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
   })
 
-  return { app, vite }
+  return { app }
 }
 
 if (!isTest) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
-// in order to trigger ssr server restart
 export default defineConfig({
-  resolve: {
-    alias: {
-      vue: resolve(process.cwd(), 'node_modules/vue'),
-    },
-  },
-  ssr: {
-    external: ['vue'],
-  },
+ 
+  
 })
